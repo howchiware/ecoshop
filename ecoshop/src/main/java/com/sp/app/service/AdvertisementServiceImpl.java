3efd46b4c5cdde7e190320aa7f455fc70c4afe80
Hjy02 <ckach13@gmail.com>
2025-08-19 00:11:18 +0900

    황정연 - 광고 신청

--- ecoshop/src/main/java/com/sp/app/service/AdvertisementServiceImpl.java
+++ ecoshop/src/main/java/com/sp/app/service/AdvertisementServiceImpl.java
@@ -1,69 +1,69 @@
-package com.sp.app.service;
-
-import java.util.Objects;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.sp.app.common.StorageService;
-import com.sp.app.exception.StorageException;
-import com.sp.app.mapper.AdvertisementMapper;
-import com.sp.app.model.Advertisement;
-
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class AdvertisementServiceImpl implements AdvertisementService {
-	private final AdvertisementMapper mapper;
-	private final StorageService storageService;
+	package com.sp.app.service;
 	
-	@Override
-	public void insertAdvertisement(Advertisement dto, String uploadPath) throws Exception {
-		try {
-			long seq = mapper.advertisingSeq();
-			dto.setAdvertisingId(seq);
-			
-			mapper.insertAdvertisement(dto);
-
-			if (! dto.getSelectFile().isEmpty()) {
-				advertisementFile(dto, uploadPath);
-			}
-			
-		} catch (Exception e) {
-			log.info("insertAdvertisement : ", e);
-			
-			throw e;
-		}
+	import java.util.Objects;
+	
+	import org.springframework.stereotype.Service;
+	import org.springframework.web.multipart.MultipartFile;
+	
+	import com.sp.app.common.StorageService;
+	import com.sp.app.exception.StorageException;
+	import com.sp.app.mapper.AdvertisementMapper;
+	import com.sp.app.model.Advertisement;
+	
+	
+	import lombok.RequiredArgsConstructor;
+	import lombok.extern.slf4j.Slf4j;
+	
+	@Service
+	@RequiredArgsConstructor
+	@Slf4j
+	public class AdvertisementServiceImpl implements AdvertisementService {
+		private final AdvertisementMapper mapper;
+		private final StorageService storageService;
 		
-	}
-
-	protected void advertisementFile(Advertisement dto, String uploadPath) throws Exception {
-		for(MultipartFile mf : dto.getSelectFile()) {
+		@Override
+		public void insertAdvertisement(Advertisement dto, String uploadPath) throws Exception {
 			try {
-				String saveFilename = Objects.requireNonNull(storageService.uploadFileToServer(mf, uploadPath));
+				long seq = mapper.advertisingSeq();
+				dto.setAdvertisingId(seq);
 				
-				String originalFilename = mf.getOriginalFilename();
-				long fileSize = mf.getSize();
+				mapper.insertAdvertisement(dto);
+	
+				if (! dto.getSelectFile().isEmpty()) {
+					advertisementFile(dto, uploadPath);
+				}
 				
-				dto.setOriginalFilename(originalFilename);
-				dto.setSaveFilename(saveFilename);
-				dto.setFileSize(fileSize);
-				
-				mapper.insertAdvertisementFile(dto);
-				
-			} catch (NullPointerException e) {
-			} catch (StorageException e) {
 			} catch (Exception e) {
+				log.info("insertAdvertisement : ", e);
+				
 				throw e;
 			}
+			
 		}
+	
+		protected void advertisementFile(Advertisement dto, String uploadPath) throws Exception {
+			for(MultipartFile mf : dto.getSelectFile()) {
+				try {
+					String saveFilename = Objects.requireNonNull(storageService.uploadFileToServer(mf, uploadPath));
+					
+					String originalFilename = mf.getOriginalFilename();
+					long fileSize = mf.getSize();
+					
+					dto.setOriginalFilename(originalFilename);
+					dto.setSaveFilename(saveFilename);
+					dto.setFileSize(fileSize);
+					
+					mapper.insertAdvertisementFile(dto);
+					
+				} catch (NullPointerException e) {
+				} catch (StorageException e) {
+				} catch (Exception e) {
+					throw e;
+				}
+			}
+			
+		}
+	
+		
 		
 	}
-
-	
-	
-}
