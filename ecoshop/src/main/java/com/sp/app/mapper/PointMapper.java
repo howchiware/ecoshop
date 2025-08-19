b8a809d70e0cd83eb0e147040c0dac290dd8dc7c
yoyo8070 <yubini322@gmail.com>
2025-08-19 23:26:30 +0900

    도유빈-포인트 모델 추가

---
+++ ecoshop/src/main/java/com/sp/app/mapper/PointMapper.java
@@ -0,0 +1,24 @@
+package com.sp.app.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.sp.app.model.Point;
+
+
+
+@Mapper
+public interface PointMapper {
+	
+	 // 포인트 등록
+    public void insertPoint(Point dto) throws SQLException;
+
+    // 회원의 가장 최근 포인트 1건 (잔액용)
+    public Point findRecentPoint(Long memberId);
+
+    // 회원의 전체 포인트 이력
+    public List<Point> listMemberPoints(Long memberId);
+	
+}
