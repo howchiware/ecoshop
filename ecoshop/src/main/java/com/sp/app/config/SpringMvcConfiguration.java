9b35e62351996bdf5e6473189056f0791175ba7c
haupyon <howni0630@gmail.com>
2025-08-21 12:10:50 +0900

    김하은 - 소켓

---
+++ ecoshop/src/main/java/com/sp/app/config/SpringMvcConfiguration.java
@@ -0,0 +1,41 @@
+package com.sp.app.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.sp.app.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class SpringMvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		List<String> excludePaths = new ArrayList<>();
+		excludePaths.add("/");
+		excludePaths.add("/dist/**");
+		excludePaths.add("/uploads/**");
+		excludePaths.add("/member/login");
+		excludePaths.add("/member/logout");
+		excludePaths.add("/member/account");
+		excludePaths.add("/member/userIdCheck");
+		excludePaths.add("/member/complete");
+		excludePaths.add("/member/pwdFind");
+		excludePaths.add("/products/**");
+		excludePaths.add("/gonggu/**");
+		excludePaths.add("/event/**");
+		excludePaths.add("/workshop/**");
+		excludePaths.add("/challenge/**");
+		excludePaths.add("/free/**");
+		excludePaths.add("/cusmotmer/**");
+		excludePaths.add("/tip/**");
+		
+		excludePaths.add("/favicon.ico");
+		
+		registry.addInterceptor(new LoginCheckInterceptor()).excludePathPatterns(excludePaths);
+	}
+
+}
