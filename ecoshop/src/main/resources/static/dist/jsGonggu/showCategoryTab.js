77d78a6b46c7cfbc0d5dcfb6a7df22703338db6f
woosu <woosu@DESKTOP-JUNG>
2025-09-03 00:47:36 +0900

    정우선-공동구매 수정

--- ecoshop/src/main/resources/static/dist/jsGonggu/showCategoryTab.js
+++ ecoshop/src/main/resources/static/dist/jsGonggu/showCategoryTab.js
@@ -24,7 +24,8 @@ $(document).ready(function() {
 $('.owl-carousel').owlCarousel({
 	loop : true,
 	margin : 10,
-	nav : true,
+	nav : false,
+	dots: false,
 	center : true,
 	autoplay:true,
 	autoplayTimeout:4000,
